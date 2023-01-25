f8e3f1f3c2d542281c06e87c6d2fb224d9cdb521
danielTran <dungtranquocdev@gmail.com>
2023-01-25 18:42:51 +0700

    highlight-status

--- app/Models/SliderModel.php
+++ app/Models/SliderModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+
 class SliderModel extends Model
 {
     const CREATED_AT = 'created';
@@ -17,11 +18,12 @@ class SliderModel extends Model
     {
         $result = null;
         if ($options['task'] == "admin-list-items") {
-            $result = self::select('*')
-                // ->where("id", ">", 4)
-                ->orderBy("id","desc")
+            $query = self::select('id', 'name', 'description', 'link', 'thumb', 'created', 'created_by', 'modified', 'modified_by', 'status');
+            if (isset($params['filter']['status']) && $params['filter']['status'] != 'all') {
+                $query->where("status", "=", $params['filter']['status']);
+            }
+            $result = $query->orderBy("id", "desc")
                 ->paginate(1);
-                // ->get();
         }
         return $result;
     }
